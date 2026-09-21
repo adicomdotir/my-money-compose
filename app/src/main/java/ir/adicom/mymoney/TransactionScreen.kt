c5e522640939385c24588af57bfe73fe53f89f0f
adicomdotir <adicom.ir@gmail.com>
2026-09-21 11:50:05 +0330

    feat: improve TransactionScreen

--- app/src/main/java/ir/adicom/mymoney/TransactionScreen.kt
+++ app/src/main/java/ir/adicom/mymoney/TransactionScreen.kt
@@ -4,10 +4,9 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -20,7 +19,9 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -41,63 +42,80 @@ fun TransactionScreen(
     val uiState by viewModel.state.collectAsStateWithLifecycle()
     val operationState by viewModel.operationState.collectAsStateWithLifecycle()
 
-    Column(
-        modifier = modifier.fillMaxSize()
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("My Money")
+                }
+            )
+        },
+        floatingActionButton = {
+            ElevatedButton(
+                onClick = {
+                    onOpenAdd()
+                }
+            ) {
+                Text("Add Transaction")
+            }
+        }
     ) {
-        Text("My Money")
-        HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
-        Text("Balance")
-        Text("$ ${uiState.transactions.calculateBalance()}")
-        Text("This Month")
-        HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
 
 
-        if (!uiState.error.isNullOrBlank()) {
-            Text("${uiState.error}")
-        } else
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(16.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text("Balance")
+                Text("$ ${uiState.transactions.calculateBalance()}")
+            }
+            Text("This Month")
+            HorizontalDivider(Modifier, DividerDefaults.Thickness, DividerDefaults.color)
 
-            if (uiState.isLoading) {
-                CircularProgressIndicator()
+
+            if (!uiState.error.isNullOrBlank()) {
+                Text("${uiState.error}")
             } else
 
-                if (uiState.transactions.isNotEmpty()) {
-                    LazyColumn(
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        items(uiState.transactions) { it ->
-                            TransactionItem(
-                                transaction = it,
-                                onClick = {
-                                    viewModel.onEvent(TransactionEvent.DeleteTransaction(it))
-                                },
-                                onDetailClick = { id ->
-                                    onDetailClick(id)
-                                },
-                                deleteEnabled = operationState !is OperationState.Deleting
-                            )
+                if (uiState.isLoading) {
+                    CircularProgressIndicator()
+                } else
+
+                    if (uiState.transactions.isNotEmpty()) {
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            items(uiState.transactions) { it ->
+                                TransactionItem(
+                                    transaction = it,
+                                    onClick = {
+                                        viewModel.onEvent(TransactionEvent.DeleteTransaction(it))
+                                    },
+                                    onDetailClick = { id ->
+                                        onDetailClick(id)
+                                    },
+                                    deleteEnabled = operationState !is OperationState.Deleting
+                                )
+                            }
                         }
+                    } else {
+                        Text("No Content")
                     }
-                } else {
-                    Text("No Content")
-                }
 
-        when (val operation = operationState) {
-            OperationState.Idle -> Unit
-            OperationState.Adding -> CircularProgressIndicator()
-            OperationState.Deleting -> CircularProgressIndicator()
-            OperationState.Updating -> CircularProgressIndicator()
-            is OperationState.Error -> Text(operation.message)
+            when (val operation = operationState) {
+                OperationState.Idle -> Unit
+                OperationState.Adding -> CircularProgressIndicator()
+                OperationState.Deleting -> CircularProgressIndicator()
+                OperationState.Updating -> CircularProgressIndicator()
+                is OperationState.Error -> Text(operation.message)
+            }
         }
-
-
-
-        ElevatedButton(onClick = {
-            onOpenAdd()
-        }) {
-            Text("Go to add transaction screen")
-        }
-
-        Spacer(modifier = Modifier.height(64.dp))
     }
 }
 
